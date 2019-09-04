dce2bcd40d702592d2b56d8ef5fd7bb1831db55a
Steven Swanson <stevenjswanson@users.noreply.github.com>
2019-09-03 23:48:15 -0700

    Do the most important thing first.

--- setup.sh
+++ setup.sh
@@ -6,10 +6,12 @@ else
     SUDO=
 fi
 
-$SUDO apt-get update
-$SUDO apt-get install -y emacs git man less curl 
-
 git config --global user.email "swanson@cs.ucsd.edu"
 git config --global user.name "Steven Swanson"
 git config --global push.default simple
+
+$SUDO apt-get update
+$SUDO apt-get install -y emacs git man less curl 
+
+
 
